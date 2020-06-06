d00e4fec741760ab1604bdc6c9d5af41024bd267
Jamie Mansfield <jmansfield@cadixdev.org>
2020-06-06 19:51:42 +0100

    Correct PackExtras.Availability

--- ftbmeta/extra/extra.go
+++ ftbmeta/extra/extra.go
@@ -7,17 +7,17 @@ import (
 type PackExtras struct {
 	Overrides *PackOverrides `json:"overrides"`
 
-	Advisories map[string][]*ftbmeta.Advisory `json:"advisories"`
-	Links      map[string]string              `json:"links"`
-	Servers    []*Server                      `json:"servers"`
+	Advisories   map[string][]*ftbmeta.Advisory `json:"advisories"`
+	Links        map[string]string              `json:"links"`
+	Servers      []*Server                      `json:"servers"`
+	Availability *ftbmeta.Availability          `json:"availability"`
 }
 
 type PackOverrides struct {
 	// The pack itself
-	Synopsis    string                 `json:"synopsis"`
-	Description string                 `json:"description"`
-	ExtraTags   []*ftbmeta.Tag         `json:"+tags"`
-	Availability *ftbmeta.Availability `json:"availability"`
+	Synopsis    string         `json:"synopsis"`
+	Description string         `json:"description"`
+	ExtraTags   []*ftbmeta.Tag `json:"+tags"`
 
 	// The pack's versions
 	Changelogs map[string]string `json:"changelogs"`
